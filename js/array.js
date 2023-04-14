76a43fd5fd60ddd5e1aa8254f6c8d19559168058
Amauri Junior <105667344+juninhods@users.noreply.github.com>
2023-04-14 12:41:05 -0300

    Add files via upload

---
+++ js/array.js
@@ -0,0 +1,13 @@
+var letras = ["essa morena", " me deixa louco", " e a", " loira deixa pirado"];
+
+function Funcao() {
+    var letra = document.getElementById("letra");
+    letra.innerHTML = letras;
+}
+
+var letras1 = ["sou pobre louco", " e vim do pouco", " sei que a vida", " é um sopro"];
+
+function Funcao1() {
+    var letra1 = document.getElementById("letra1");
+    letra1.innerHTML = letras1;
+}
